626d0a1ba2bda483659775c227ca6ee21c2f8b3f
Aniket Dhaygude <aniketdhaygude4@gmail.com>
2024-09-14 17:00:55 +0530

    Actual Project With Background & ForeGround

---
+++ src/components/ForeGround.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ForeGround() {
+  return (
+    <>
+      <div className="fixed top-0 left-0 z-[3] w-full h-screen "></div>
+    </>
+  );
+}
+
+export default ForeGround;
