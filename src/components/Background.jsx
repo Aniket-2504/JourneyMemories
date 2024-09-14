40304bd85e09a6d18fda6cf7dca3d90a9880cdb9
Aniket Dhaygude <aniketdhaygude4@gmail.com>
2024-09-14 18:15:49 +0530

    Static Card is Done

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -4,7 +4,7 @@ function Background() {
   return (
     <>
         <div className='fixed z-[2] w-full h-screen'>
-            <div className=' absolute top-[5%] w-full py-10 flex justify-center font-semibold text-2xl text-yellow-500'>Pages</div>
+            <div className=' absolute top-[5%] w-full py-10 flex justify-center font-semibold text-2xl text-yellow-500'>Cards</div>
             <h1 className=' absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[10vw] text-zinc-300 leading-none font-semibold'>Note.</h1>
         </div>
     </>
