4c0c65d96fd8040be58b4e86bafe811b2bbdc71c
Aniket Dhaygude <aniketdhaygude4@gmail.com>
2024-09-15 17:11:51 +0530

    Added dynmaic card with props & framer motion for drag Effect

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,32 +2,48 @@ import React from 'react'
 import { IoDocumentTextOutline } from 'react-icons/io5'
 import { FaRegFileAlt } from 'react-icons/fa'
 import { IoMdDoneAll } from 'react-icons/io'
+import { motion } from "framer-motion"
 
-function Card() {
+function Card({data, reference}) {
   return (
 
     
-    <div className="relative w-60 h-72 rounded-[40px] bg-zinc-200 px-5 py-10 overflow-hidden">
+    <motion.div drag  dragConstraints={reference} whileDrag={{scale: 1.1}} dragElastic={0.1} className="relative flex-shrink-0 w-60 h-72 rounded-[40px] bg-zinc-200 px-5 py-10 overflow-hidden">
         <FaRegFileAlt/>
         <p className='text-sm mt-5  leading-tight'>
-            I want to visit Kedarnath befor I turned 21.
+            {data.decs}
             </p>
 
             <div className="footer absolute bottom-0  w-full  left-0  ">
 
-                    <div className='flex items-center justify-between mb-2 py-3 px-5'>
-                        <h4 className='font-semibold'>Done  </h4>
+              {data.close ? (<div className='flex items-center justify-between mb-2 py-3 px-5'>
+                        <h4 className='font-semibold'>Done </h4>
                         <span className='w-5 h-5 bg-green-700 rounded-full flex items-center justify-center shadow-md '>
                         {/* <IoMdDoneAll color='#fff' /> */}
                         </span>
                         
-                    </div>
-                    <div className='tag w-full py-5 bg-yellow-400 flex items-center justify-center'>
-                        <h3 className='text-sm font-semibold'>Working in Progress</h3>
-                    </div>
+                    </div>)
+                     : (<div className='flex items-center justify-between mb-2 py-3 px-5'>
+                        <h4 className='font-semibold'>Progress... </h4>
+                        <span className='w-5 h-5 bg-yellow-400 rounded-full flex items-center justify-center shadow-md '>
+                        {/* <IoMdDoneAll color='#fff' /> */}
+                        </span>
+                        
+                    </div>) }
+
+                    
+                    
+                    {data.tag.isOpen ? ( <div className='tag w-full py-5 bg-green-700 flex items-center justify-center'>
+                        <h3 className='text-sm font-semibold'>I made it</h3>
+                    </div>) 
+                    :  (<div className='tag w-full py-5 bg-yellow-400 flex items-center justify-center'>
+                    <h3 className='text-sm font-semibold'>Working in Progress</h3>
+                </div>)}
+
+                   
             </div>
 
-    </div>
+    </motion.div>
     
 
   )
--- src/components/ForeGround.jsx
+++ src/components/ForeGround.jsx
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useRef } from "react";
 import Card from "./Card";
 
+
 function ForeGround() {
+
+  const ref = useRef(null);
+
+  const data =[
+    {
+      decs: "I want to make 6 figures before 2024 ends",
+      close: true,
+      tag: { isOpen: true, tagTitle: "Work in Progress", tagColor: "green",}
+    },
+
+    {
+      decs: "I want to visit kedarnath before I turn 21",
+      close: false,
+      tag: { isOpen: false, tagTitle: "Work in Progress", tagColor: "green",}
+    },
+
+    {
+      decs: "I want to start investment before 2024 ends",
+      close: false,
+      tag: { isOpen: false, tagTitle: "Work in Progress", tagColor: "green",}
+    },
+
+  ]
+
   return (
     <>
-      <div className="fixed top-0 left-0 z-[3] w-full h-screen ">
-            <Card/>
+      <div ref={ref} className="fixed top-0 left-0 z-[3] w-full h-full gap-10 flex flex-wrap p-5">
+        {data.map((item, index)=>(
+              <Card data={item} reference={ref}/>
+        ))}
+          
       </div>
     </>
   );
